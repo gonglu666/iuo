acba241a4ff4a2fc12158505f5e3312c6146b6b5
kaka <g514331301@163.com>
2019-12-14 16:05:39 +0800

    fgg

---
+++ main/java/com/example/demo/Wer.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * Created by kaonglu
+ * 2019/12/14
+ */
+public class Wer {
+    private final static String kk = "dg";
+
+    public static void main(String[] args) {
+
+    }
+}
